aff06095e471784a58a46bebcd56d4f3511eaf27
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2022-11-26 10:28:48 +0100

    Creada actividad item y clase adapter

--- app/src/main/java/com/itziar/restclient/MainActivity.java
+++ app/src/main/java/com/itziar/restclient/MainActivity.java
@@ -1,14 +1,36 @@
 package com.itziar.restclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recycler;
+    private AdapterRecycler adapter;
+
+    private static final String[] myDataSet = {
+            "PHP",
+            "Javascript",
+            "Go",
+            "Python"
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recycler = findViewById(R.id.listTracks);
+        recycler.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recycler.setLayoutManager(layoutManager);
+
+        adapter = new AdapterRecycler(myDataSet);
+        recycler.setAdapter(adapter);
     }
 }
